56700451fcf2dc3405bac3ccf8f4639c7ba7c5b4
Nayeon Keum <rmaskdus0208@gmail.com>
2023-04-05 11:04:55 +0900

    [feat/#10] Add: master api(mkdir, cronjob, etc) with test

--- resources/images/master-ubuntu/server/getAssignments.py
+++ resources/images/master-ubuntu/server/getAssignments.py
@@ -1,22 +1,32 @@
-# This is executed by a cron job
-
+#!/usr/bin/python3
+# This file is executed by a cron job
 import json
 import requests
+import sys
+import res.Dto as Dto
+import res.Urls as Urls
+import base64
 
-# 학생 api list가 있어서, 여기서 받은 거 다 내 디렉토리에 파일화함
-#  ~/강의명/과제명/학번
+assignment_id = sys.argv[1]
+json_str = requests.get(Urls.base_url+"?assignment_id="+assignment_id).text
+data_users_assigned = list(json.loads(json_str))  # json to list[dict]
 
+dir_path = Urls.dir_path_master
+dir_path_slave = Urls.dir_path_slave
 
-dir_path = "/Users/nkeum/1914395"
-url = "http://localhost:8001/files/L1VzZXJzL25rZXVtL3Rlc3Q="
-json_str = requests.get(url).text
-# json_str = "{\"2.py\": \"import os\\nimport json\\n\\n\\nprint(\\\"Hello World\\\")\\n\", \"1.txt\": \"sldkf\\nsadklfj\\ndsfksld\\nsdlkfjskd\\n\\n\"}"
+for user in data_users_assigned:
+    # mkdir in slave
+    encoded_dir_path_slave = base64.b64encode((
+        dir_path_slave+"/"+str(assignment_id)).encode('ascii')).decode('ascii')  # base64 encode
+    url = Urls.slave_base_url+"/files/"+encoded_dir_path_slave
+    json_str = requests.get(url).text
+    # json to dict
+    result = json.loads(json.loads(json_str))
 
-# json to dict
-result = json.loads(json.loads(json_str))
-
-# write files
-for filename, str in result.items():
-    f = open(dir_path+"/"+filename, 'w')
-    f.write(str)
-    f.close()
+    # write files
+    for filename, content in result.items():
+        assignment_dir_path = "%s/%s/%d/%s" % (
+            dir_path, assignment_id, user.id, filename)
+        f = open(assignment_dir_path, 'w')
+        f.write(content)
+        f.close()
