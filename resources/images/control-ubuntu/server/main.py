27a5c87f6a63d6a21b19cb0f38639ca8b9dd44e4
Nayeon Keum <rmaskdus0208@gmail.com>
2023-05-09 15:40:53 +0900

    [feat/#24] Fix: k8s apiserver authentication(from pod) for request done

--- resources/images/control-ubuntu/server/main.py
+++ resources/images/control-ubuntu/server/main.py
@@ -20,27 +20,29 @@ def yaml_to_json(id, isProfessor):
         role = "student"
     f = open("./res/remote-deploy-%s.yml" % role, 'r')
     # apply env var
-    file = f.read().replace("{{ ID }}", str(id))
+    ID, PORT = str(id), str(8887+id)
+    file = f.read().replace("{{ ID }}", ID)
+    file = file.replace("{ PORT }", PORT)
     contents = file.split("---")
-    for i in range(len(contents)-1):
+    for i in range(len(contents)):
         with open("./res/tmp%d.yml" % i, "w") as tmp:
             tmp.write(contents[i])
 
     # convert yaml to json
-    for i in range(len(contents)-1):
+    for i in range(len(contents)):
         with open("./res/tmp%d.yml" % i, 'r') as yaml_in, open("./res/tmp%d.json" % i, "w") as json_out:
             yaml_object = yaml.safe_load(yaml_in)
             json.dump(yaml_object, json_out)
 
-    return len(contents)-1  # object file num
+    return len(contents)  # object file num
 
 
 @app.post("/professor/{professor_info}")
-@validate
-async def create_professor_res(professor_info: Dto.UsersModel):
-    id = professor_info.id
+async def create_professor_res(professor_info: str):  # Dto.UsersModel):
+    # pid = professor_info.id
+    pid = 0
 
-    json_num = yaml_to_json(id, True)
+    json_num = yaml_to_json(pid, True)
 
     for i in range(json_num):
         json_str = json.load(open(
@@ -49,16 +51,18 @@ async def create_professor_res(professor_info: Dto.UsersModel):
             url = Urls.kube_base_url+"/api/v1/namespaces/professor-ns/pods"
         elif json_str['kind'] == "Service":
             url = Urls.kube_base_url+"/api/v1/namespaces/professor-ns/services"
-        result = requests.post(url, json=json_str).text
+        headers = {"Authorization": "Bearer %s" % (os.getenv('TOKEN')),
+                   'Accept': 'application/json', "Content-Type": "application/json"}
+        result = requests.post(url, data=json.dumps(json_str),
+                               headers=headers, verify=os.getenv('CACERT')).text
         if result == False:  # TODO deliver error message
-            print("Error in creating professor, id:"+id)
+            print("Error in creating professor, id:"+pid)
         else:
             print("success")
 
 
 @app.post("/lecture/{lecture_info}")
-@validate
-async def create_lecture(lecture_info: Dto.LectureModel, data_users_assigned):
+async def create_lecture(lecture_info: str):  # Dto.LectureModel
     json_str = requests.get(
         Urls.base_url+"?lecture_id="+lecture_info.lecture_id).text
     data_users_assigned = list(json.loads(json_str))  # json to list[dict]
@@ -68,13 +72,15 @@ async def create_lecture(lecture_info: Dto.LectureModel, data_users_assigned):
         id = user.id
         json_num = yaml_to_json(id, False)
         for i in range(json_num):
-            json_str = json.load(open(
-                "./res/tmp%d.json" % i))
+            json_str = json.load(open("./res/tmp%d.json" % i))
             if json_str['kind'] == "Pod":
                 url = Urls.kube_base_url+"/api/v1/namespaces/student-ns/pods"
             elif json_str['kind'] == "Service":
                 url = Urls.kube_base_url+"/api/v1/namespaces/student-ns/services"
-            result = requests.post(url, json=json_str).text
+            headers = {"Authorization": "Bearer %s" % (os.getenv('TOKEN')),
+                       'Accept': 'application/json', "Content-Type": "application/json"}
+            result = requests.post(url, data=json.dumps(json_str),
+                                   headers=headers, verify=os.getenv('CACERT')).text
             if result == False:  # TODO deliver error message
                 print("Error in creating student, id:"+id)
             else:
