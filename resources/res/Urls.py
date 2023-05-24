a7a1138c81e95df773d3d269b606192e927d918d
Nayeon Keum <rmaskdus0208@gmail.com>
2023-05-24 21:25:11 +0900

    [feat/#36] Add: api getting assignment content

--- resources/res/Urls.py
+++ resources/res/Urls.py
@@ -6,4 +6,4 @@ student_base_url = os.getenv('STUDENT_BASE_URL')  # "http://localhost:8001"
 # student_base_url = "http://student-ubuntu-svc.student-ns.svc.cluster.local:8001"
 
 dir_path_student = os.getenv('DIR_PATH_STUDENT')  # "/root/assignment"
-dir_path_professor = os.getenv('DIR_PATH_PROFESSOR')  # "/Users/nkeum/test"
+dir_path_professor = os.getenv('DIR_PATH_PROFESSOR')  # "/root/assignment"
