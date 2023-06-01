da30c99089d15f6be4a56ed7f7848fd4e9649b37
Nayeon Keum <rmaskdus0208@gmail.com>
2023-06-01 17:47:52 +0900

    [feat/#41] Fix: apply real urls

--- resources/images/control-ubuntu/server/res/Urls.py
+++ resources/images/control-ubuntu/server/res/Urls.py
@@ -1,7 +1,9 @@
 import os
 
-kube_base_url = os.getenv('KUBE_BASE_URL')
-base_url = os.getenv('BASE_URL')  # "http://"  # db server
+kube_api_server = os.getenv('APISERVER')  # https://kubernetes.default.svc
+kube_base_url = os.getenv('KUBE_BASE_URL')  # http://172... ## 8080
+
+base_url = os.getenv('BASE_URL')  # "http://moaroom-back.duckdns.org:8080"
 student_base_url = os.getenv('STUDENT_BASE_URL')  # "http://localhost:8001"
 # student_base_url = "http://student-ubuntu-svc.student-ns.svc.cluster.local:8001"
 
