e8fefc906f00e28e303b8042c4a847707ed5a688
Nayeon Keum <nkeum@lablup.com>
2023-08-31 20:46:35 +0900

    [feat/#84] Refactor: string to format

--- resources/images/professor-ubuntu/server/getAssignments.py
+++ resources/images/professor-ubuntu/server/getAssignments.py
@@ -11,7 +11,7 @@ assignment_id = sys.argv[1]
 lecture_id = sys.argv[2]
 
 json_str = requests.get(
-    Urls.base_url+"/assignments/"+assignment_id+"/urls").text
+    f"{Urls.base_url}/assignments/{assignment_id}/urls").text
 data_users_assigned = list(json.loads(json_str))  # json to list[dict]
 
 dir_path = Urls.dir_path_professor
@@ -20,18 +20,17 @@ dir_path_student = Urls.dir_path_student
 for user in data_users_assigned:
     # mkdir in student
     encoded_dir_path_student = base64.b64encode((
-        dir_path_student+"/"+assignment_id).encode('ascii')).decode('ascii')  # base64 encode
+        f"{dir_path_student}/{assignment_id}").encode('ascii')).decode('ascii')  # base64 encode
     urlmodel = json.loads(requests.get(
-        Urls.base_url+"/users/"+user["id"]+"/"+lecture_id+"/url").text)
-    url = urlmodel["apiEndpoint"]+"/files/" + encoded_dir_path_student
+        f"{Urls.base_url}/users/{user['id']}/{lecture_id}/url").text)
+    url = f"{urlmodel['apiEndpoint']}/files/{encoded_dir_path_student}"
     json_str = requests.get(url).text
     # json to dict
     result = json.loads(json.loads(json_str))
 
     # write files
     for filename, content in result.items():
-        assignment_dir_path = "%s/%s/%s/%s" % (
-            dir_path, assignment_id, user["id"], filename)
+        assignment_dir_path = f"{dir_path}/{assignment_id}/{user['id']}/{filename}"
         f = open(assignment_dir_path, 'w')
         f.write(content)
         f.close()
