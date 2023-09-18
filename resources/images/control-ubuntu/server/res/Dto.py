0e68a1bb2c39d916d113c1a60b1b9327c3f9c0cf
Nayeon Keum <nkeum@lablup.com>
2023-09-18 23:33:29 +0900

    [main] Hotfix: revert dto changes

--- resources/images/control-ubuntu/server/res/Dto.py
+++ resources/images/control-ubuntu/server/res/Dto.py
@@ -35,7 +35,7 @@ class AssignmentModel(BaseModel):
     start_date: List[int]
     due_date: List[int]
     description: Optional[str]
-    answer: List[str]
+    answer: str
     runtime: float
 
 
