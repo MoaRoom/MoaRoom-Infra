8aed9a41a34c103c510551c81c9f4f09dcdb59da
Nayeon Keum <nkeum@lablup.com>
2023-09-29 18:20:10 +0900

    [feat/#97] Fix: extension support

--- resources/images/professor-ubuntu/server/getValueAndTime_c.sh
+++ resources/images/professor-ubuntu/server/getValueAndTime_c.sh
@@ -3,10 +3,10 @@
 start=`date +%s.%N`
 
 /usr/bin/gcc $1 -o $2/test.out
-$2/test.out > values.txt
+$2/test.out > $2/values.txt
 
 finish=`date +%s.%N`
 
 
 diff=$( echo "$finish - $start" | bc -l )
-/usr/bin/echo $diff > time.txt
+/usr/bin/echo $diff > $2/time.txt
--- resources/images/professor-ubuntu/server/getValueAndTime_cpp.sh
+++ resources/images/professor-ubuntu/server/getValueAndTime_cpp.sh
@@ -3,10 +3,10 @@
 start=`date +%s.%N`
 
 /usr/bin/g++ -o $2/test $1
-$2/test > values.txt
+$2/test > $2/values.txt
 
 finish=`date +%s.%N`
 
 
 diff=$( echo "$finish - $start" | bc -l )
-/usr/bin/echo $diff > time.txt
+/usr/bin/echo $diff > $2/time.txt
